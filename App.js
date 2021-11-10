e408d2d04aa00c900342659cbe479f04d785666f
zaheersani <sani.zaheer@gmail.com>
2021-11-10 19:15:55 +0500

    ScrollView and FlatList | TODO App Basics

--- App.js
+++ App.js
@@ -1,91 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  FlatList,
+  ToastAndroid,
+} from 'react-native';
 
-const App = () => {
-  const [op, setOp] = React.useState(0)
-  const [discount, setDiscount] = React.useState(0)
-  const [save, setSave] = React.useState(0)
-
-  React.useEffect(() => {
-    console.log("Component Rendered!")
-    setSave(op / 100 * discount)
-  })
-
-  const calcOp = (n) => {
-    setOp(n)
-    // setSave(op / 100 * discount)
-  }
-  
-  const calcDiscount = (n) => {
-    setDiscount(n)
-    // setSave(op / 100 * discount)
-  }
-
+const ScrollViewDemo = () => {
+  var arr = [34, 45, 34, 23, 56, 67, 78, 56, 45, 45, 34, 5, 56, 5, 67];
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        // onChangeText={(val) => setOp(val)}
-        onChangeText={(val) => calcOp(val)}
-        // value={getText}
-        placeholder="Original Price"
-        keyboardType={'number-pad'}
-      />
-      <TextInput
-        style={styles.input}
-        // onChangeText={(val) => setDiscount(val)}
-        onChangeText={(val) => calcDiscount(val)}
-        // value={getText}
-        placeholder="Discount %"
-        keyboardType={'number-pad'}
-      />
-      <Text style={styles.text}>Original Price: {op}</Text>
-      <Text style={styles.text}>Discount %: {discount}</Text>
-      <Text style={styles.text}>You Saved: {save}</Text>
-      <Text style={styles.text}>Final Price: {op - save}</Text>
-      <TouchableOpacity
-        onPress={() => setSave(op/100*discount)}
-        style={
-          {
-            marginTop: 10,
-            backgroundColor: 'skyblue',
-            width: 150,
-            height: 50,
-            justifyContent: 'center',
-            borderRadius: 10,
-            elevation: 5,
-          }}
+      <ScrollView
+        style={{ padding: 10 }}
+      // horizontal={true}
       >
-        <Text style={{ fontSize: 24, color: 'white', alignSelf: 'center'}}>Calculate</Text>
-      </TouchableOpacity>
+        {arr.map((i, index) => (
+          <Text style={{ fontSize: 45 }}>
+            item: {i}, index: {index}
+          </Text>
+        ))}
+      </ScrollView>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create(
-  {
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      fontSize: 28,
-      color: '#897'
-    },
-    input: {
-      borderColor: 'blue',
-      borderWidth: 1,
-      borderRadius: 5,
-      width: 150,
-      height: 30,
-      paddingLeft: 10,
-      margin: 5,
-      fontSize: 24
-    }
-  }
-);
+const App = () => {
+  var arr = [34, 45, 34, 23, 56, 67, 78, 56, 45, 45, 34, 5, 56, 5, 67];
+  const [getText, setText] = React.useState('');
+  const [getItems, setItems] = React.useState([
+    { data: 'Item 1', key: 1 },
+    { data: 'Item 2', key: 3 },
+  ]);
+  const addItem = () => {
+    setItems([...getItems, { data: getText, key: Math.random() }]);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={{ flexDirection: 'row' }}>
+        <TextInput
+          style={{ fontSize: 16, borderColor: 'blue', borderWidth: 1 }}
+          placeholder="Enter Item"
+          onChangeText={setText}
+        />
+        <Button title="Add" onPress={addItem} />
+      </View>
+      <FlatList
+        style={{ paddingTop: 20 }}
+        data={getItems}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            key={item.key}
+            style={{
+              backgroundColor: 'red',
+              margin: 5,
+              padding: 5,
+              borderRadius: 15,
+              width: 200,
+            }}>
+            <Text style={{ fontSize: 25 }}>item: {item.data}</Text>
+            <Text style={{ fontSize: 25 }}>index: {index}</Text>
+            <Text style={{ fontSize: 25 }}>key: {item.key}</Text>
+          </TouchableOpacity>
+        )}
+        // numColumns={2}
+        ListEmptyComponent={<Text>There is not Data in the List</Text>}
+        ListHeaderComponent={<Text style={{ fontSize: 20 }}>TODO Items</Text>}
+        ListFooterComponent={
+          <Text style={{ fontSize: 20 }}>This is the Footer of FlatList</Text>
+        }
+        refreshing={false}
+        onRefresh={() =>
+          ToastAndroid.show('fetching data...', ToastAndroid.SHORT)
+        }
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
+  },
+});
 
 export default App;
